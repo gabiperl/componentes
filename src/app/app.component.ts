084a97706ad49f95b45ab7c334c6d9ee6233fecd
Gabi <gabriel.perl.1996@gmail.com>
2025-04-06 19:52:57 +0200

    Estructura y creación de servicios. Listo para comenzar a programar.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from "./footer/footer.component";
+import { HeaderComponent } from "./header/header.component";
+import { NavBarComponent } from "./nav-bar/nav-bar.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FooterComponent, HeaderComponent, NavBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { SearchComponent } from './sections/search/search.component';
+import { CartComponent } from './sections/cart/cart.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    {path: 'home', component: SearchComponent},
+    {path: 'cart', component: CartComponent},
+    {path: '**', redirectTo: 'home'}
+
+];
