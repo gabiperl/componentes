bb548ebc8b5f085b5727145093bb2fdaf85afe8f
Gabi <gabriel.perl.1996@gmail.com>
2025-04-10 15:12:13 +0200

    Muestra de filtros y de todos los artículos

--- src/app/sections/cart/article-card/article-card.component.ts
+++ src/app/sections/cart/article-card/article-card.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ConexionAPIService } from '../../../conexion-api.service';
 
 @Component({
   selector: 'app-article-card',
@@ -7,5 +8,10 @@ import { Component } from '@angular/core';
   styles: ``
 })
 export class ArticleCardComponent {
+  @Input() datos:any
+
+  constructor(private api:ConexionAPIService) { }
+
+  getArticles(){return this.api.getArticulos();}
 
 }
--- src/app/sections/search/search.component.ts
+++ src/app/sections/search/search.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { ConexionAPIService } from '../../conexion-api.service';
+import { ArticleCardComponent } from '../cart/article-card/article-card.component';
 
 @Component({
   selector: 'app-search',
-  imports: [],
+  imports: [ArticleCardComponent],
   templateUrl: './search.component.html',
   styles: ``
 })
 export class SearchComponent {
+
+  constructor(private api:ConexionAPIService) { }
+
+
+  getCategorias() {return this.api.getCategorias();}
+  getFabricantes() {return this.api.getFabricantes();}
+  getArticles() {return this.api.getArticulos();}
 
 }
